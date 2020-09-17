6392e73236c7bd92f934ecb5a43114c9ffd75405
Sinis2101 <sinisterra2101@gmail.com>
2020-09-17 17:41:14 -0500

    Se añadieron los siguientes procedimientos. Proc 1: retornar un arreglo con el precio menor de cada material. Proc 2: retornar un arreglo con el lugar que tiene el menor precio de cada material. Proc 3: retornar el tamaño del arreglo que contendrá los materiales según la categoría solicitada por el usuario. Proc 4: retornar un arreglo que contendrá los materiales según la categoría solicitada por el usuario. Proc 5: retornar la suma de los mejores precios.
    Se añadieron las siguientes funciones.
    Func 1: mostrar el mejor precio de cada material y en donde se compra.
    Func 2: solicitar la ubicacion y retornar el costo del domicilio.
    Func 3: mostrar el total de la obra con los mejores precios.
    
    Se finalizo el main, integrando todas las funciones y procedimientos.

--- src/model/Budget.java
+++ src/model/Budget.java
@@ -2,19 +2,73 @@ package model;
 
 public class Budget {
 	
-	public static void printList(int[] list) {
+	public static void printList(String[] list) {
 		for(int i=0; i<list.length; i++) {
 			System.out.println(list[i]);
 		}
 	}
 	
-	public static int totalPrice(int[] price, int[] amount, int workforce) {
+	public static int totalPrice(int[] price, int[] amount) {
 		int total = 0;
 		for(int i=0; i<price.length; i++) {
 			total += price[i]*amount[i];
 		}
-		total += workforce;
 		return total;
 	}
+		
+	public static int[] bestPrice(int[] priceList1, int[] priceList2, int[] priceList3) {
+		int[] bestPriceList = new int[priceList1.length];
+		for(int i=0; i<priceList1.length; i++) {
+			if(priceList1[i]<=priceList2[i]&&priceList1[i]<=priceList3[i]) {
+				bestPriceList[i] = priceList1[i];
+			} else if(priceList2[i]<=priceList1[i]&&priceList2[i]<=priceList3[i]) {
+				bestPriceList[i] = priceList2[i];
+			} else if(priceList3[i]<=priceList1[i]&&priceList3[i]<=priceList2[i]){
+				bestPriceList[i] = priceList3[i];
+			}
+		}
+		return bestPriceList;
+	}
+
+	public static String[] bestPricePlace(int[] bestPriceList, int[] priceList1, int[] priceList2, int[] priceList3) {
+		String[] bestPricePlaceList = new String[bestPriceList.length];
+		for(int i=0; i<bestPriceList.length; i++) {
+			if(bestPriceList[i]==priceList1[i]) {
+				bestPricePlaceList[i]="Homecenter";
+			}else if(bestPriceList[i]==priceList2[i]) {
+				bestPricePlaceList[i]="Ferreteria del centro";
+			}else{
+				bestPricePlaceList[i]="Ferreteria del barrio";
+			}
+		}
+		return bestPricePlaceList;
+	} 
+			
+	public static int getFilterListSize(String[] useList, String filter) {
+		int size = 0;
+		for(int i=0;i<useList.length;i++) {
+			if(useList[i].equalsIgnoreCase(filter)) {
+				size += 1;
+			}
+		}
+		return size;
+	}
 	
+	public static String[] getFilterList(int size, String[] materialList, String[] useList, String filter) {
+		String[] filterList = new String[size];
+		for(int i=0;i<useList.length;i++) {
+			if(useList[i].equalsIgnoreCase(filter)) {
+				filterList[i] = materialList[i];
+			}
+		}
+		return filterList;
+	}
+	
+	public static int getBestTotal(int[] bestPriceList, int[] amountList) {
+		int total = 0;
+		for(int i=0; i<bestPriceList.length;i++) {
+			total += bestPriceList[i]*amountList[i];
+		}
+		return total;
+	}
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -15,7 +15,7 @@ public class Main {
 		int numberOfMaterials = sc.nextInt();
 		sc.nextLine();
 		return numberOfMaterials;
-	}		
+	}
 	public static String[] getMaterialName(int neededMaterials, Scanner sc) {
 		String[] materialName = new String[neededMaterials];
 		for (int i=0; i<neededMaterials; i++) {
@@ -41,16 +41,59 @@ public class Main {
 		}
 		return materialUse;		
 	}
-	public static int[] getMaterialPrice(int neededMaterials, String[] materials, Scanner sc, String tienda) {
+	public static int[] getMaterialPrice(int neededMaterials, String[] materials, Scanner sc, String shop) {
 		int[] materialPrice = new int[neededMaterials];
 		for (int i=0; i<neededMaterials; i++) {
-			System.out.println("Ingrese el precio de "+materials[i]+" en "+tienda+":");
+			System.out.println("Ingrese el precio de "+materials[i]+" en "+shop+":");
 			materialPrice[i] = sc.nextInt();
 		}
 		sc.nextLine();
 		return materialPrice;
 	}
-
+	public static void showBestPrice(String[] material, int[] bestPriceList, String[] bestPricePlaceList) {
+		for(int i=0; i<material.length; i++) {
+			System.out.println("El mejor precio para "+material[i]+" es "+bestPriceList[i]+" en "+bestPricePlaceList[i]);
+		}
+	}
+	public static int deliveryCost(String location, int totalPrice) {
+		int cost = 0;
+		location = location.toLowerCase();
+		switch(location) {
+			case "norte":
+				if(totalPrice<80000) {
+					cost = 120000;
+				}else if(totalPrice<300000) {
+					cost = 28000;
+				}else {
+					cost = 0;
+				}				
+				break;
+			case "centro":
+				if(totalPrice<80000) {
+					cost = 50000;
+				}else if(totalPrice<300000) {
+					cost = 0;
+				}else {
+					cost = 0;
+				}
+				break;	
+			case "sur":
+				if(totalPrice<80000) {
+					cost = 120000;
+				}else if(totalPrice<300000) {
+					cost = 55000;
+				}else {
+					cost = 0;
+				}
+				break;	
+		}
+		return cost;
+	}
+	public static void showTotalPerShop(int materialPrice, int deliveryPrice, int workforce, String shop) {
+		int total = materialPrice+deliveryPrice+workforce;
+		System.out.println("El costo total de la obra si los materiales se compran en "+shop+" es: "+total);
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int totalMaterial = totalMaterials(sc);
@@ -60,8 +103,27 @@ public class Main {
 		int[] pricesHomeCenter = getMaterialPrice(totalMaterial, materialList, sc, "HomeCenter");
 		int[] pricesFerreteriaCentro = getMaterialPrice(totalMaterial, materialList, sc, "Ferreteria del centro");
 		int[] pricesFerreteriaBarrio = getMaterialPrice(totalMaterial, materialList, sc, "Ferreteria del barrio");
-		System.out.println("El valor total de los materiales en Homecenter es: "+Budget.totalPrice(pricesHomeCenter, amountList, WORKFORCE));
-		System.out.println("El valor total de los materiales en Ferreteria del centro es: "+Budget.totalPrice(pricesFerreteriaCentro, amountList, WORKFORCE));
-		System.out.println("El valor total de los materiales en Ferreteria del barrio es: "+Budget.totalPrice(pricesFerreteriaBarrio, amountList, WORKFORCE));
+		int materialTotalHomeCenter = Budget.totalPrice(pricesHomeCenter, amountList);
+		int materialTotalFerreteriaCentro = Budget.totalPrice(pricesFerreteriaCentro, amountList);
+		int materialTotalFerreteriaBarrio = Budget.totalPrice(pricesFerreteriaBarrio, amountList);
+		System.out.println("Ingrese la ubicacion de su inmueble. Norte, centro o sur?");
+		String location = sc.nextLine();
+		int deliveryCostHomeCenter = deliveryCost(location, materialTotalHomeCenter);
+		int deliveryCostFerreteriaCentro = deliveryCost(location, materialTotalFerreteriaCentro);
+		int deliveryCostFerreteriaBarrio = deliveryCost(location, materialTotalFerreteriaBarrio);
+		showTotalPerShop(materialTotalHomeCenter, deliveryCostHomeCenter, WORKFORCE, "HomeCenter");
+		showTotalPerShop(materialTotalFerreteriaCentro, deliveryCostFerreteriaCentro, WORKFORCE, "la Ferreteria del Centro");
+		showTotalPerShop(materialTotalFerreteriaBarrio, deliveryCostFerreteriaBarrio, WORKFORCE, "la Ferreteria del Barrio");
+		int[] bestPriceList = Budget.bestPrice(pricesHomeCenter, pricesFerreteriaCentro, pricesFerreteriaBarrio);
+		String[] bestPricePlaceList = Budget.bestPricePlace(bestPriceList, pricesHomeCenter, pricesFerreteriaCentro, pricesFerreteriaBarrio);
+		showBestPrice(materialList, bestPriceList, bestPricePlaceList);
+		int bestTotal = Budget.getBestTotal(bestPriceList, amountList);
+		int bestTotalDeliveryCost = deliveryCost(location, bestTotal);
+		System.out.println("El costo total de la obra si los materiales se compran al mejor precio es: "+(bestTotal+bestTotalDeliveryCost+WORKFORCE));
+		System.out.println("Ingrese una categoria para ver sus materiales. Obra negra, Obra Blanca o Pintura?");
+		String filter = sc.nextLine();
+		int filterListSize = Budget.getFilterListSize(useList, filter);
+		String[] filterList = Budget.getFilterList(filterListSize, materialList, useList, filter);
+		Budget.printList(filterList);
 	}
 }
